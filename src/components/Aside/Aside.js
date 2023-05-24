7b028760b52e4965d3b39812a30f4425139fe844
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-23 19:29:53 -0700

    Aside component added with styles

---
+++ src/components/Aside/Aside.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Aside.css';
+
+export default function Aside() {
+    return (
+        <aside className='Aside'>
+            <div>Vending Machine for your daily needs</div>
+            <div>Don't be shy try it our.</div>
+            <div>I don't bite</div>
+        </aside>
+    )
+}
