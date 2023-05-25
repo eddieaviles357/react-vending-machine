5d03b507f246279316fb6b0b37e04be2d52a2ba2
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-24 19:37:19 -0700

    imported BrowserRouter that wraps around VendingMachine component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 import VendingMachine from './VendingMachine';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <VendingMachine />
-    </div>
+    </BrowserRouter>
   );
 }
 
