33faeaca840b2fb7376e01a13754537a37930daa
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-24 19:42:58 -0700

    Simply renders the VendingMachine component

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import VendingItems from './VendingItems';
+
+export default function Home(props) {
+    return (
+        <div>
+            <VendingItems />
+        </div>
+    )
+};
